fb94e8e516c6a941247dbef789d00a2c50bdb390
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2022-03-24 18:30:31 +0100

    feat: wip Socket

---
+++ pages/api/socket.js
@@ -0,0 +1,36 @@
+import { Server } from "Socket.IO";
+const tmi = require("tmi.js");
+
+const SocketHandler = (req, res) => {
+  const client = new tmi.client({
+    options: { debug: true },
+    connection: {
+      reconnect: true,
+      secure: true,
+    },
+    identity: {
+      username: "botmannzone",
+      password: "oauth:huhwdyv8k23j0vbu55jkkxtfpxa7wa",
+    },
+    channels: ["baumannzone"],
+  });
+
+  client.connect();
+
+  if (res.socket.server.io) {
+    console.log("Socket is already running");
+  } else {
+    console.log("Socket is initializing");
+    const io = new Server(res.socket.server);
+    res.socket.server.io = io;
+
+    client.on("message", (channel, tags, message, self) => {
+      if (self) return;
+
+      io.emit("chat", { channel, tags, message });
+    });
+  }
+  res.end();
+};
+
+export default SocketHandler;
